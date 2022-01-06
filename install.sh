24b86d8f1805a0f56bd0d2f517bc647ddb7986b0
Emil Bertlin <75381427+emilbertlin@users.noreply.github.com>
2022-01-06 23:20:29 +0100

    Only added a comment

--- install.sh
+++ install.sh
@@ -4,6 +4,16 @@ chmod +x ./install_packages.sh ./stow_packages.sh
 
 ./install_packages.sh
 
+# nvm.fish has an error when you have no folder in ~/.local/share/nvm but this seems to fix it.
+
+if [ -d ~/.local/share/nvm]
+
+then
+    :
+else
+    mkdir ~/.local/share/nvm
+fi
+
 ./stow_packages.sh
 
 # Vim/neovim plugin install with vim-plug
