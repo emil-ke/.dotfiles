230949d941b625253ad4e3cea3ca476e6a9f79ee
Emil Bertlin <75381427+emilbertlin@users.noreply.github.com>
2022-01-08 21:23:26 +0100

    made  options more readable

--- nvim/.config/nvim/lua/basics.lua
+++ nvim/.config/nvim/lua/basics.lua
@@ -1,25 +1,50 @@
-vim.g.mapleader = ' '
-vim.o.number  = true
-vim.o.relativenumber = true
-vim.o.wrap = false
-vim.o.expandtab = true 
-vim.o.incsearch = true
-vim.o.tabstop = 2
-vim.o.cursorline = true
-vim.o.ignorecase = true
-vim.o.hlsearch = false
-vim.o.swapfile = false
-vim.o.splitbelow = true
-vim.o.splitright = true
-vim.o.scrolloff = 3
-vim.o.errorbells = false
-vim.o.shiftwidth = 2
-vim.o.numberwidth = 4
-vim.o.termguicolors = true
-vim.o.showtabline = 6 
-vim.o.signcolumn = 'yes'
-vim.o.showmode = false
-vim.o.mouse = 'a'
+vim.g.mapleader = ','
+
+local options = {
+  backup = false,
+  clipboard = 'unnamedplus',
+  cmdheight = 2,
+  conceallevel = 0,
+  fileencoding = 'utf-8',
+  pumheight = 10,
+  number  = true,
+  relativenumber = true,
+  wrap = false,
+  expandtab = true ,
+  incsearch = true,
+  tabstop = 2,
+  cursorline = true,
+  ignorecase = true,
+  hlsearch = false,
+  swapfile = false,
+  splitbelow = true,
+  splitright = true,
+  scrolloff = 8,
+  errorbells = false,
+  shiftwidth = 2,
+  numberwidth = 4,
+  termguicolors = true,
+  showtabline = 4,
+  signcolumn = 'yes',
+  showmode = false,
+  mouse = 'a',
+  smartindent = true,
+  timeoutlen = 100,
+  undofile = true,
+  updatetime = 300,
+  writebackup = false,
+  sidescrolloff = 8,
+}
+
+vim.opt.shortmess:append 'c'
+
+for k, v in pairs(options) do
+	vim.opt[k] = v
+end
+
+vim.cmd 'set whichwrap+=<,>,[,],h,l'
+vim.cmd [[set iskeyword+=-]]
+vim.cmd [[set formatoptions-=cro]] --TODO: this doesn't seem to work
 
 vim.api.nvim_set_keymap('n', 'vs', ':vs<CR>', { noremap = true })
 vim.api.nvim_set_keymap('n', 'sp', ':sp<CR>', { noremap = true })
@@ -36,13 +61,13 @@ vim.api.nvim_set_keymap('n', '<C-N>', ':Lexplore<CR> :vertical resize 30<CR>', {
 vim.api.nvim_set_keymap('n', '<leader>t', ':sp<CR> :term<CR> :resize 20N<CR> i', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('t', '<Esc>', '<C-\\><C-n>', { noremap = true, silent = true })
 
--- move lines up or down with alt-(up/down)
+-- better whitespace
+vim.api.nvim_set_keymap('n', 'öä', ':StripWhitespace<CR>', { noremap = true, silent = true })
+-- move line up or down with alt-(up/down)
 vim.api.nvim_set_keymap('n', '<A-Down>', ':m .+1<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<A-Up>', ':m .-2<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('v', '<A-Down>', ':m .+1<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('v', '<A-Up>', ':m .-2<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<A-Up>', ':m .-1<CR>', { noremap = true, silent = true })
 
-
-vim.g['netrw_banner'] = 0
-vim.g['netrw_liststyle'] = 3
 vim.g['netrw_winsize'] = 25
+vim.g['netrw_liststyle'] = 3
